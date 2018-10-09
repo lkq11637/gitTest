7fc4280a0a4a5d0b515d647c953f86246be4b3be
lkq <1163768006@qq.com>
2018-10-09 14:54:28 +0800

    test

---
+++ test1/src/com/xt/test/Test1.java
@@ -0,0 +1,11 @@
+package com.xt.test;
+
+/**
+ * @author shkstart
+ * @create 2018-10-09 14:53
+ */
+public class Test1 {
+    public static void main(String[] args){
+        System.out.println("hello,world!");
+    }
+}
